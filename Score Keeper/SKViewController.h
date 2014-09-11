0af24f4e38e388c9162f1a0eaf3b85495ce35db1
Joseph Aranda <joseph.aranda21@gmail.com>
2014-09-10 20:50:33 -0600

    SKViewer
    Buttons

---
+++ Score Keeper/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  Score Keeper
+//
+//  Created by Joseph Aranda on 9/10/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
